2c89d6539fd6850dacf0a394515a8bf6cbd999af
Dmitry Novoselov <n9-vesel@mail.ru>
2025-10-29 22:17:19 +0300

    homework 12_2

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,37 @@
+import logging
+
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/masks.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(num_card: str) -> str:
-    """Функция, которая вовращает номер карты ввиде ХХХХ ХХ** **** ХХХХ"""
+    """Функция, которая возвращает номер карты в виде ХХХХ ХХ** **** ХХХХ"""
+    logger.debug(f"Ввод карты: {num_card}")
     num_card = str(num_card)
     if len(num_card) == 16:
         mask_card = f"{num_card[:4]} {num_card[4:6]}** **** {num_card[-4:]}"
+        logger.debug(f"Маскировка вводимой карты {mask_card}")
         return mask_card
+    logger.error("ВВели некорректный номер карты")
     return "Некорректный ввод"
 
 
 def get_mask_account(acc_num: str) -> str:
-    """Функция, которая возвращает номер счета ввиде **ХХХХ"""
+    """Функция, которая возвращает номер счета в виде **ХХХХ"""
+    logger.debug(f"Ввод номера счета {acc_num}")
     acc_num = str(acc_num)
     if len(acc_num) >= 4:
         mask_acc = f"**{acc_num[-4:]}"
+        logger.debug(f"Маскировка счета {mask_acc}")
         return mask_acc
+    logger.error("Ввели некорректный номер счета")
     return "Некорректный ввод"
+
+
+# get_mask_card_number(1234567890123456)
+# get_mask_account(12345678456)
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,27 @@
 import json
+import logging
+
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/utils.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def read_file(path):
     """Функция, которая читает json файл"""
+    logger.debug("Начало работы функции")
     try:
+        logger.debug("Открытие json-файла")
         with open(path, "r", encoding="utf-8") as json_file:
             try:
                 data = json.load(json_file)
-            except json.JSONDecodeError:
+            except json.JSONDecodeError as ex:
+                logger.error(f"Невалидный json файл. Ошибка: {ex}")
                 return []
     except FileNotFoundError:
         return []
+    logger.debug("Завершение работы функции")
     return data
-
-
-# Тест
-# print(read_file('../data/operations.json'))
